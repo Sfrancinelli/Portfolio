65364d7f25fd27131c45c034a2e6d2b0628a90a4
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-26 10:23:11 -0300

    media settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,6 +142,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ["static/"]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
