38360d80a532b4f1857ba61ee755e0b87da5d78f
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-26 11:51:38 -0300

    url y view for index

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from portfolio import views as p_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', p_views.index, name='index')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ portfolio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User, Permission
+from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import AnonymousUser
+
+
+def index(request):
+    return render(request, 'pages/index.html', {'title': 'Portfolio de Sebastián Francinelli'})
