9a5b87d8aaaffb5039149a8e0b861dcee8b3906b
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-26 10:23:25 -0300

    first model for project management

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class ProjectTag(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.project.title} - {self.tag.name}"
