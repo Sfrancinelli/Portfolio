f10d53b502f1b5eef798b2d9a9d5eed2f058d70b
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-29 11:37:41 -0300

    url for login

--- administracion/urls.py
+++ administracion/urls.py
@@ -4,10 +4,10 @@ from administracion import views
 urlpatterns = [
 
     path('', views.index_administracion, name='index_admin'),
+    path('login/', views.login, name="login"),
     # path('orentacion/', views.orientacion_index, name='orientacion_index'),
     # path('orientacion/nuevo/', views.orientacion_nuevo, name='orientacion_nuevo'),
     # path('orientacion/editar/<int:id_orientacion>', views.orientacion_editar, name='orientacion_editar'),
     # path('orientacion/eliminar/<int:id_orientacion>', views.orientacion_eliminar, name='orientacion_eliminar'),
     # path('orientacion/buscar/', views.orientacion_buscar, name='buscar_orientaciones'),
-
 ]
