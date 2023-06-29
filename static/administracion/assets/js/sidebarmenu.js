d18dbb47ab06d092040916452dd6512979dbe8a2
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-29 15:57:00 -0300

    sidebar menu update

--- static/administracion/assets/js/sidebarmenu.js
+++ static/administracion/assets/js/sidebarmenu.js
@@ -1,12 +1,3 @@
-/*
-Template Name: Admin Template
-Author: Wrappixel
-
-File: js
-*/
-// ==============================================================
-// Auto select left navbar
-// ==============================================================
 $(function () {
     "use strict";
     var url = window.location + "";
