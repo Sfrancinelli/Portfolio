d599ae7bcef7a3c3296887db7f3ca3cbf339462d
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-29 11:37:47 -0300

    view for login

--- administracion/views.py
+++ administracion/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_administracion(request):
     return render(request, 'administracion/index.html')
+
+
+def login(request):
+    return render(request, 'administracion/authentication-login.html')
