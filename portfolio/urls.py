09aa9e69647b92e97546170fa8287bb02f9d22e6
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-06-29 10:30:16 -0300

    included administracion.urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from portfolio import views as p_views
+from administracion import views as a_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', p_views.index, name='index'),
     path('about/', p_views.about, name='about'),
     path('contact/', p_views.contact, name='contact'),
+    path('administracion/', include('administracion.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
