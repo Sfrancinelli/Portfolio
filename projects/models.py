8f0a86b06953dbc65b2868040cab5c32ffbb5935
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-05 10:04:10 -0300

    added Category model with many to many from Project

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Categorías"
+        verbose_name_plural = "Categorías"
+
     def __str__(self):
         return self.name
 
