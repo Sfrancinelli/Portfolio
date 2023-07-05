2379173eaf344259d2f19e8e5b74a920cf0b4d53
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-05 10:03:53 -0300

    added category model into admin

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from projects.models import Project, Tag, ProjectTag
+from projects.models import Project, Tag, ProjectTag, Category
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 
@@ -27,10 +27,16 @@ class ProjectTagAdmin(admin.ModelAdmin):
     list_display_links = ('id', )
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ('id', )
+
+
 my_admin = MyAdmin(name='SF')
 my_admin.register(Project, ProjectsAdmin)
 my_admin.register(Tag, TagAdmin)
 my_admin.register(ProjectTag, ProjectTagAdmin)
+my_admin.register(Category, CategoryAdmin)
 
 my_admin.register(User, UserAdmin)
 my_admin.register(Group, GroupAdmin)
